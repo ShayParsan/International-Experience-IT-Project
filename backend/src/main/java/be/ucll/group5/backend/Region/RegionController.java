6f40c88961107e38be63404755fcae45483ac2e8
JustJNow <146000339+JustJNow@users.noreply.github.com>
2025-03-10 17:17:16 +0100

    Adding requestmapping

--- backend/src/main/java/be/ucll/group5/backend/Region/RegionController.java
+++ backend/src/main/java/be/ucll/group5/backend/Region/RegionController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequestMapping("/regions")
 public class RegionController {
     @Autowired
     private RegionService regionService;
--- backend/src/main/java/be/ucll/group5/backend/Reward/RewardController.java
+++ backend/src/main/java/be/ucll/group5/backend/Reward/RewardController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rewards")
 public class RewardController {
     @Autowired
     private RewardService rewardService;
