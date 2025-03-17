6cb57073670a6d9ca94fe835b8516ebceeddc1b2
ShayParsan <Shay.Parsan@student.ucll.be>
2025-03-17 17:11:11 +0100

    Update RewardController.java

--- backend/src/main/java/be/ucll/group5/backend/Reward/RewardController.java
+++ backend/src/main/java/be/ucll/group5/backend/Reward/RewardController.java
@@ -1,9 +1,12 @@
 package be.ucll.group5.backend.Reward;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Reward Management", description = "APIs for managing rewards")
 @RestController
 @RequestMapping("/rewards")
 public class RewardController {
@@ -14,16 +17,19 @@ public class RewardController {
         this.rewardService = rewardService;
     }
 
+    @Operation(summary = "Get all rewards", description = "Retrieves a list of all rewards")
     @GetMapping
     public List<Reward> getRewards() {
         return rewardService.getRewards();
     }
 
+    @Operation(summary = "Add a new reward", description = "Creates a new reward and saves it to the database")
     @PostMapping
     public Reward addReward(@RequestBody Reward reward) {
         return rewardService.addReward(reward);
     }
 
+    @Operation(summary = "Delete a reward", description = "Removes a reward by its ID")
     @DeleteMapping("/{id}")
     public void removeReward(@PathVariable Long id) {
         rewardService.removeReward(id);
